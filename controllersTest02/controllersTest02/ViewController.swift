06d08d28ff26752cb51970181b81565b22e3ea21
fedir72 <fedir72@ua.fm>
2021-07-09 19:09:15 +0300

    empty project 2

---
+++ controllersTest02/controllersTest02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  controllersTest02
+//
+//  Created by fedir on 09.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
