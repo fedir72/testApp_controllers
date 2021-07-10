73ba607284c1e1ffb668e63b35fee97004841be4
fedir72 <fedir72@ua.fm>
2021-07-10 12:16:25 +0300

    task2, case1

---
+++ controllersTest02/controllersTest02/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  controllersTest02
+//
+//  Created by fedir on 09.07.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    //MARK: - outlets
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+
+   //MARK: - viewDidLoad()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+    }
+    
+   
+}
+    
+
+   
+
--- controllersTest02/controllersTest02/ViewController.swift
+++ controllersTest02/controllersTest02/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
+    //MARK: - outlets
+    
+    @IBOutlet private weak var textField: UITextField!
+    
+    //MARK: - viewDidLoad()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        self.textField.delegate = self
     }
 
-
+    //MARK: - actions
+    
+    @IBAction func didtapColorButtons(_ sender: UIButton) {
+        
+    let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "SecondVC") as SecondViewController
+        switch sender.tag {
+        
+        case 1: vc.title = "Green"
+        case 2: vc.title = "Red"
+        default: vc.title = "Blue"
+        }
+        
+        vc.view.backgroundColor = sender.backgroundColor
+        vc.textLabel.text = textField.text
+        navigationController?.pushViewController(vc, animated: true)
+        
+    }
+    
 }
 
+//MARK: - UITextFieldDelegate
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.textField.resignFirstResponder()
+        //self.view.endEditing(true)
+        return true
+    }
+    
+}
